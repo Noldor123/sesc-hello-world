cb24a82a2617c2bb07479fab63551f2ac3e5ed4a
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-09-22 10:41:13 +0700

    Update program.c

--- program.c
+++ program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (){
   char str [30];
-  if (scanf("%s", str)!=0){
+  if (scanf("%s", str)!=EOF){
     printf("Hello, %s!", str);
     }else{
       printf("Hello, nameless!");
