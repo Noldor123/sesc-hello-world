ca652b141d7e2a8c8580d84aa59325dcff34f907
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-09-22 09:18:20 +0700

    Create program.c

---
+++ program.c
@@ -0,0 +1,11 @@
+include "stdio.h"
+int main (){
+  char str [30];
+  if (scanf("%s", str)!=0){
+    printf("Hello, %s!", str);
+    }else{
+      printf("Hello, nameless!");
+    }
+  }
+  return 0;
+}
